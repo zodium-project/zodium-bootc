770de079cda7171e278f49e8c36601319dbeec39
zodium#dev <dev.zodium.project@gmail.com>
2026-02-05 13:58:03 +0530

    switch to script

---
+++ files/scripts/remove-fedora-kde-themes.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+cd /usr/share/plasma/look-and-feel/
+pwd
+ls
+rm -rf /usr/share/plasma/look-and-feel/org.fedoraproject.fedora.desktop/
+rm -rf /usr/share/plasma/look-and-feel/org.fedoraproject.fedoradark.desktop/
+rm -rf /usr/share/plasma/look-and-feel/org.fedoraproject.fedoralight.desktop/
+ls
