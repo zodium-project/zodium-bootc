3c7371c26886ba7ebe924d7be78a7803cd6f5456
zodium#dev <dev.zodium.project@gmail.com>
2026-02-05 18:53:02 +0530

    remove fedora branding

--- files/scripts/remove-fedora-icon-themes.sh
+++ files/scripts/remove-fedora-icon-themes.sh
@@ -10,3 +10,20 @@ rm -rf /usr/share/icons/Bluecurve/
 rm -rf /usr/share/icons/locolor/
 rm -rf /usr/share/icons/AdwaitaLegacy/
 ls
+cd /usr/share/icons/hicolor/
+rm -rf /usr/share/icons/hicolor/16x16/apps/fedora-logo-icon.png
+rm -rf /usr/share/icons/hicolor/22x22/apps/fedora-logo-icon.png
+rm -rf /usr/share/icons/hicolor/24x24/apps/fedora-logo-icon.png
+rm -rf /usr/share/icons/hicolor/32x32/apps/fedora-logo-icon.png
+rm -rf /usr/share/icons/hicolor/36x36/apps/fedora-logo-icon.png
+rm -rf /usr/share/icons/hicolor/48x48/apps/fedora-logo-icon.png
+rm -rf /usr/share/icons/hicolor/96x96/apps/fedora-logo-icon.png
+rm -rf /usr/share/icons/hicolor/256x256/apps/fedora-logo-icon.png
+rm -rf /usr/share/icons/hicolor/16x16/places/start-here.png
+rm -rf /usr/share/icons/hicolor/22x22/places/start-here.png
+rm -rf /usr/share/icons/hicolor/24x24/places/start-here.png
+rm -rf /usr/share/icons/hicolor/32x32/places/start-here.png
+rm -rf /usr/share/icons/hicolor/36x36/places/start-here.png
+rm -rf /usr/share/icons/hicolor/48x48/places/start-here.png
+rm -rf /usr/share/icons/hicolor/96x96/places/start-here.png
+rm -rf /usr/share/icons/hicolor/256x256/places/start-here.png
