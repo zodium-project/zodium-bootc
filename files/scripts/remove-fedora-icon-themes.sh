770e8100ee88dca8ae4c7567d965735af32f583d
zodium#dev <dev.zodium.project@gmail.com>
2026-02-05 14:44:13 +0530

    remove icons bloat

---
+++ files/scripts/remove-fedora-icon-themes.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+cd /usr/share/icons/
+pwd
+ls
+rm -rf /usr/share/icons/oxygen/
+rm -rf /usr/share/icons/Bluecurve/
+rm -rf /usr/share/icons/locolor/
+rm -rf /usr/share/icons/AdwaitaLegacy/
+ls
