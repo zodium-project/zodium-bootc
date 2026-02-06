cc5a1727ff5722e0dbca59e5dd13fc2fa7a0b8d1
zodium#dev <dev.zodium.project@gmail.com>
2026-02-06 12:46:05 +0530

    add flathub

---
+++ files/scripts/remove-fedora-flatpak.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+flatpak remotes
+flatpak remote-delete fedora
+flatpak remotes
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-modify --no-filter --enable flathub
+flatpak remotes
