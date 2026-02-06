61212b4b6b5cf2370e3c4b1e4e6030f891a91d19
zodium#dev <dev.zodium.project@gmail.com>
2026-02-06 19:56:46 +0530

    fix 2

--- files/scripts/install-flathub.sh
+++ files/scripts/install-flathub.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 set -x
 
 flatpak remotes
-flatpak remote-delete fedora
-flatpak remotes
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak remote-modify --no-filter --enable flathub
 flatpak remotes
