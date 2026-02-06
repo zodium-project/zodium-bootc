33753baa2fe94ea5aad905238a43bc1df960d403
zodium <dev.zodium.project@gmail.com>
2026-02-06 22:01:18 +0530

    upstream changes .

--- files/scripts/install-flathub.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-set -x
-
-flatpak remotes
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak remote-modify --no-filter --enable flathub
-flatpak remotes
--- files/system/usr/libexec/gamemode-hwp-end.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-HWP="/sys/devices/system/cpu/intel_pstate/hwp_dynamic_boost"
-
-if [[ -w "$HWP" ]]; then
-    echo 0 > "$HWP"
-fi
