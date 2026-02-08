f024c25ad9655568eba7d61ff5838712686c18a1
zodium#dev <dev.zodium.project@gmail.com>
2026-02-08 16:17:15 +0530

    better method

--- files/scripts/set-default-wallpapers.sh
+++ files/scripts/set-default-wallpapers.sh
@@ -10,3 +10,10 @@ do
   sudo sed -i '/^\[Wallpaper\]/,/^\[/{s/^Image=.*/Image=Zodium/}' \
   /usr/share/plasma/look-and-feel/$lf/contents/defaults
 done
+
+echo "Default wallpapers set to Zodium (verify by checking the output below):"
+cat /usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/defaults | grep Image
+cat /usr/share/plasma/look-and-feel/org.kde.breezedark.desktop/contents/defaults | grep Image
+cat /usr/share/plasma/look-and-feel/org.kde.breezetwilight.desktop/contents/defaults | grep Image
+
+echo "Done setting default wallpapers to Zodium."
