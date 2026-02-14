3e1dcde1f911750b1538bfc295663fcf0f7ef1ee
zodium <dev.zodium.project@gmail.com>
2026-02-14 22:30:20 +0530

    reduce retundancy

--- files/scripts/purge-fedora-branding.sh
+++ files/scripts/purge-fedora-branding.sh
@@ -2,6 +2,12 @@
 set -euo pipefail
 set -x
 
+rm -rf /usr/share/plasma/look-and-feel/org.fedoraproject.fedora.desktop/
+rm -rf /usr/share/plasma/look-and-feel/org.fedoraproject.fedoradark.desktop/
+rm -rf /usr/share/plasma/look-and-feel/org.fedoraproject.fedoralight.desktop/
+rm -rf /usr/share/sddm/themes/01-breeze-fedora/
+rm -rf /usr/share/wallpapers/F*
+rm -rf /usr/share/wallpapers/D*
 rm -rf /usr/share/icons/oxygen/
 rm -rf /usr/share/icons/Bluecurve/
 rm -rf /usr/share/icons/locolor/
