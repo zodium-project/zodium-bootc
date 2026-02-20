10cbb35718c73d7376777a3fe8cae028e8283a9b
zodium#dev <dev.zodium.project@gmail.com>
2026-02-20 17:07:11 +0530

    tweaks/fix

--- files/scripts/set-kde-wallpapers.sh
+++ files/scripts/set-kde-wallpapers.sh
@@ -6,12 +6,6 @@ for lf in \
   org.kde.breezedark.desktop \
   org.kde.breezetwilight.desktop
 do
-  sudo sed -i '/^\[Wallpaper\]/,/^\[/{s/^Image=.*/Image=zodium/}' \
+  sudo sed -i '/^\[Wallpaper\]/,/^\[/{s/^Image=.*/Image=miku/}' \
   /usr/share/plasma/look-and-feel/$lf/contents/defaults
 done
-
-#echo "Default wallpapers set to Zodium (verify by checking the output below):"
-#cat /usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/defaults | grep Image
-#cat /usr/share/plasma/look-and-feel/org.kde.breezedark.desktop/contents/defaults | grep Image
-#cat /usr/share/plasma/look-and-feel/org.kde.breezetwilight.desktop/contents/defaults | grep Image
-#echo "Done setting default wallpapers to Zodium."
