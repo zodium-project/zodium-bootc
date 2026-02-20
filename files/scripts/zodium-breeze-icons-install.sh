3e70e7dee5a93b9a9ade9da3e5e3762914edc698
zodium#dev <dev.zodium.project@gmail.com>
2026-02-20 13:32:16 +0530

    snapshot 6

--- files/scripts/zodium-breeze-icons-install.sh
+++ files/scripts/zodium-breeze-icons-install.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
-set -x
 
 mkdir -p /usr/share/icons/
-
 cd /usr/share/icons/
-
 curl -L -o zodium-breeze-rounded.tar.gz \
 https://github.com/zodium-project/zodium-breeze-rounded/releases/latest/download/zodium-breeze-rounded.tar.gz
-
 tar -xzf zodium-breeze-rounded.tar.gz -C /usr/share/icons
-
-ls
+rm -rf zodium-breeze-rounded.tar.gz
