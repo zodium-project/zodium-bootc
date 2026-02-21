fb97d097eaabacffa0726b6d2e3af12b8d198eac
zodium#dev <dev.zodium.project@gmail.com>
2026-02-22 01:02:53 +0530

    new wallpapers implemented

--- files/scripts/set-kde-wallpapers.sh
+++ files/scripts/set-kde-wallpapers.sh
@@ -6,6 +6,6 @@ for lf in \
   org.kde.breezedark.desktop \
   org.kde.breezetwilight.desktop
 do
-  sudo sed -i '/^\[Wallpaper\]/,/^\[/{s/^Image=.*/Image=miku/}' \
+  sudo sed -i '/^\[Wallpaper\]/,/^\[/{s/^Image=.*/Image=Castorice/}' \
   /usr/share/plasma/look-and-feel/$lf/contents/defaults
 done
