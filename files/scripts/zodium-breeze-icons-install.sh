f8507c86cb5215e23088eb245fa91ea05c4484c1
zodium-dev <dev.zodium.project>
2026-03-12 23:17:41 +0530

    fix missing in spectacle

--- files/scripts/zodium-breeze-icons-install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-mkdir -p /usr/share/icons/
-cd /usr/share/icons/
-curl -L -o zodium-breeze-rounded.tar.gz \
-https://github.com/zodium-project/zodium-breeze-rounded/releases/latest/download/zodium-breeze-rounded.tar.gz
-tar -xzf zodium-breeze-rounded.tar.gz -C /usr/share/icons
-rm -rf zodium-breeze-rounded.tar.gz
